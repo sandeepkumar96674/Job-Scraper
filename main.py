bf43680ff5ee013fe976a0d0e85cd54f17b3fe36
Sandeep Kumar <kumarsandeep110104@gmail.com>
2024-11-21 12:17:50 +0530

    Solved the port issur

--- main.py
+++ main.py
@@ -108,3 +108,4 @@ interface = gr.Interface(
 # Launch the Gradio app
 if __name__ == "__main__":
     interface.launch()
+    interface.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
